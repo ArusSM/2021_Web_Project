d0f543afde9d68cace3bb688d03eb414c83cc16f
arussm <kimjng10@naver.com>
2021-04-13 11:58:31 +0900

    색 디자인 하는 중.

--- 2021_Web_Project(front)/app.js
+++ 2021_Web_Project(front)/app.js
@@ -1,18 +1,40 @@
 window.onload = function () {
     $(".s_t").hide();
+    $(".GamePopup").hide();
+    $(".recGame").hide();
     let status = false;
     $(".openBtn").on("click", function (e) {
-        let idx = e.target.dataset.idx;
-        let open = e.target.dataset.open;
+        
+        let idx =  e.currentTarget.dataset.idx;
+        let open = e.currentTarget.dataset.open;
         if (open == "false") {
             $(".s_t").eq(idx).stop().slideDown();
-            e.target.dataset.open = true;
+            e.currentTarget.dataset.open = true;
+            $(this).html(`<i class="fas fa-minus"></i>`);
         }else {
             $(".s_t").eq(idx).stop().slideUp();
-            e.target.dataset.open = false;
+            e.currentTarget.dataset.open = false;
+            $(this).html(`<i class="fas fa-plus"></i>`);
         }
         console.log("올라가떠여")
     });
 
+    $(".card").on("click", function(e) {
+        $(".GamePopup").show("slow");
+    }) 
+
+    $(".closePop").click(function(){
+        $(".GamePopup").hide("slow");
+    });
+
+    $(".recommend").click(function(){
+        $(".recGame").show("slow");
+    })
+
+    $(".closeRec").click(function(){
+        $(".recGame").hide("slow");
+    });
+        
+    
     
 }
